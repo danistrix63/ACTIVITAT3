79ebaeb62322014dffbbf23eec7388c75458243f
dani <ddctrabajocontacto@gmail.com>
2024-11-12 18:57:38 +0100

    DOM Y SAX

--- GestorEncarrecs.java
+++ GestorEncarrecs.java
@@ -1,43 +1,111 @@
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import org.xml.sax.*;
+import org.xml.sax.helpers.DefaultHandler;
 
 public class GestorEncarrecs {
 
+    public static void generarXMLDOM(ArrayList<Encarrec> encarrecs, String filename) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.newDocument();
+
+            Element root = doc.createElement("encarrecs");
+            doc.appendChild(root);
+
+            for (Encarrec enc : encarrecs) {
+                Element encElement = doc.createElement("encarrec");
+                encElement.setAttribute("id", String.valueOf(enc.getId()));
+
+                Element client = doc.createElement("client");
+                client.appendChild(doc.createTextNode(enc.getNomClient()));
+                encElement.appendChild(client);
+
+                Element telefon = doc.createElement("telefon");
+                telefon.appendChild(doc.createTextNode(enc.getTelefon()));
+                encElement.appendChild(telefon);
+
+                Element data = doc.createElement("dataEncàrrec");
+                data.appendChild(doc.createTextNode(enc.getDataEncàrrec()));
+                encElement.appendChild(data);
+
+                root.appendChild(encElement);
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(filename));
+
+            transformer.transform(source, result);
+            System.out.println("XML generat correctament.");
+
+        } catch (Exception e) {
+            System.err.println("Error al generar XML: " + e.getMessage());
+        }
+    }
+
     public static void llegirXMLDOM(String filename) {
         try {
-            // Configuración del parser DOM
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document doc = builder.parse(new File(filename));
 
-            // Normalizar el documento XML
-            doc.getDocumentElement().normalize();
+            NodeList encarrecs = doc.getElementsByTagName("encarrec");
 
-            // Obtener todos los elementos "encarrec"
-            NodeList encarrecsList = doc.getElementsByTagName("encarrec");
+            for (int i = 0; i < encarrecs.getLength(); i++) {
+                Element encElement = (Element) encarrecs.item(i);
+                String id = encElement.getAttribute("id");
+                String client = encElement.getElementsByTagName("client").item(0).getTextContent();
+                String telefon = encElement.getElementsByTagName("telefon").item(0).getTextContent();
+                String data = encElement.getElementsByTagName("dataEncàrrec").item(0).getTextContent();
 
-            // Recorrer cada encargo y mostrar los datos
-            for (int i = 0; i < encarrecsList.getLength(); i++) {
-                Node node = encarrecsList.item(i);
-
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element encElement = (Element) node;
-
-                    String id = encElement.getAttribute("id");
-                    String client = encElement.getElementsByTagName("client").item(0).getTextContent();
-                    String telefon = encElement.getElementsByTagName("telefon").item(0).getTextContent();
-                    String dataEncàrrec = encElement.getElementsByTagName("dataEncàrrec").item(0).getTextContent();
-
-                    System.out.println("Encàrrec ID: " + id);
-                    System.out.println("Client: " + client);
-                    System.out.println("Telèfon: " + telefon);
-                    System.out.println("Data Encàrrec: " + dataEncàrrec);
-                    System.out.println("-------------------------");
-                }
+                System.out.println("ID: " + id + ", Client: " + client + ", Telèfon: " + telefon + ", Data: " + data);
             }
         } catch (Exception e) {
             System.err.println("Error al llegir XML amb DOM: " + e.getMessage());
+        }
+    }
+
+    public static void llegirXMLSAX(String filename, String clientNom) {
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+
+            DefaultHandler handler = new DefaultHandler() {
+                boolean isClient = false;
+                String currentClient = "";
+
+                @Override
+                public void startElement(String uri, String localName, String qName, Attributes attributes) {
+                    if (qName.equalsIgnoreCase("client")) {
+                        isClient = true;
+                    }
+                }
+
+                @Override
+                public void characters(char[] ch, int start, int length) {
+                    if (isClient) {
+                        currentClient = new String(ch, start, length);
+                        isClient = false;
+                    }
+                }
+
+                @Override
+                public void endElement(String uri, String localName, String qName) {
+                    if (qName.equalsIgnoreCase("client") && currentClient.equalsIgnoreCase(clientNom)) {
+                        System.out.println("Encàrrec trobat per al client: " + clientNom);
+                    }
+                }
+            };
+
+            saxParser.parse(new File(filename), handler);
+
+        } catch (Exception e) {
+            System.err.println("Error al llegir XML amb SAX: " + e.getMessage());
         }
     }
 }
